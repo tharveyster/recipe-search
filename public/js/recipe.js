b09d7345afb71c18caf78f2f85051eabe7d40105
Rajesh Gautam <rajeshgautam@Rajeshs-MacBook-Pro.local>
2021-09-02 21:31:33 -0400

    updated

--- public/js/recipe.js
+++ public/js/recipe.js
@@ -1,15 +1,45 @@
-const logout = async () => {
-    const response = await fetch('/api/users/logout', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-    });
-  
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
-    }
-  };
-  
-  document.querySelector('#logout').addEventListener('click', logout);
-  
+const getIngredients = async () => {
+  const response = await fetch('/api/users/ingedients', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  });
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
+};
+document.querySelector('#ingredients').addEventListener('click', getIngredients);
+const getTitle = async () => {
+  const response = await fetch('/api/users/title', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  });
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
+};
+document.querySelector('#title').addEventListener('click', getTitle);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
