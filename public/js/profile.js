6fcb68f2c0f7cecb679611ff64a593b2e915200c
tharvey <tharveyster@gmail.com>
2021-09-08 06:06:54 -0400

    Got the search results buttons working to return a full recipe

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,56 +1,55 @@
-const recipeList = JSON.parse(localStorage.getItem("recipeList")) || [];
-console.log(localStorage)
+const recipeList = JSON.parse(localStorage.getItem('recipeList')) || [];
+console.log(localStorage);
 
 const newFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const title = document.querySelector('#get-recipe').value;
-    const cuisine = document.querySelector('#recipe-list').value;
-  
-    if (title) {
+  event.preventDefault();
 
-     
-      const response = await fetch(`/api/recipe`, {
-        method: 'POST',
-        body: JSON.stringify({ title, cuisine }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
+  const title = document.querySelector('#get-recipe').value;
+  const cuisine = document.querySelector('#recipe-list').value;
 
-      });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert('Failed to create recipe');
-      }
+  if (title) {
+    const response = await fetch(`/api/recipe`, {
+      method: 'POST',
+      body: JSON.stringify({ title, cuisine }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to create recipe');
     }
-console.log("something")
+  }
+  console.log('something');
+};
 
-  };
-  
-  const delButtonHandler = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-      const id = event.target.getAttribute('data-id');
-  
-      const response = await fetch(`/api/recipe/${id}`, {
-        method: 'DELETE',
-      });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert('Failed to delete recipe');
-      }
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/recipe/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to delete recipe');
     }
-  };
+  }
+};
 
- 
-  document
-    .querySelector('#btn1')
-    .addEventListener('submit', newFormHandler);
-  
-  document
-    .querySelector('#btn2')
-    .addEventListener('click', delButtonHandler);
-  
+document.querySelector('#btn1').addEventListener('submit', newFormHandler);
+
+document.querySelector('#btn2').addEventListener('click', delButtonHandler);
+
+$(document).ready(function () {
+  // Event listener for recipe buttons
+  $('.titleBtn').on('click', function () {
+    let recipeIdReq = $(this).attr('data-attr');
+    document.location.replace(`/search/recipe/${recipeIdReq}`);
+  });
+
+});
